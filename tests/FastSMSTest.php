e549aba713c7c9aa6440aa2e3d47967c9a7799b8
Ruslan Tabachuk <ruslan.tabachuk@lezgro.com>
2015-05-25 14:07:28 +0300

    FSAPIPHPL-17 Add HTTP libraries. Add library to use detection. Add call methods.

--- tests/FastSMSTest.php
+++ tests/FastSMSTest.php
@@ -5,10 +5,24 @@ use Netsecrets\FastSMS\FastSMS;
 class FastSMSTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testNachHasCheese()
+    /**
+     * SDK object
+     * @var \Netsecrets\FastSMS\FastSMS
+     */
+    private $sdk;
+
+    public function __construct($name = null, array $data = array(), $dataName = '')
     {
-        $nacho = new FastSMS;
-        $this->assertTrue($nacho->hasCheese());
+        $this->sdk = new FastSMS('6Rx6-stiU-a8sY-vt9i');
+        parent::__construct($name, $data, $dataName);
+    }
+
+    /**
+     * Test is load HTTP library
+     */
+    public function testLoadHTTPLibrary()
+    {
+        $this->assertArrayHasKey($this->sdk->getHTTPLibrary(), FastSMS::getSupportHTTPLibraries());
     }
 
 }
